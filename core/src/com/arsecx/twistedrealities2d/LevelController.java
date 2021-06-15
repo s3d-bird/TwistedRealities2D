4b38eed70ee213f6bfa79c8b04d6885c529cdce1
s3d-b1rd <arslank3377@gmail.com>
2021-06-15 22:57:20 +0500

    Initial Commit

---
+++ core/src/com/arsecx/twistedrealities2d/LevelController.java
@@ -0,0 +1,20 @@
+package com.arsecx.twistedrealities2d;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class LevelController extends Game {
+	public SpriteBatch batch;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+
+}
